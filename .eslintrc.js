59c7f50ffa8312c5b875e0014cca183de0435b27
Yogiswara Utama <utama.yogiswara@gmail.com>
2021-01-13 10:30:50 +0700

    setup prettier and eslint for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['/*.*'],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
